fb8f0324fb1080e939fe70e9eae831f791b3cbe2
ztzzhi <9972792+zhangtingzhi@user.noreply.gitee.com>
2022-06-29 16:22:47 +0800

    update

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,18 @@
-/*使用typescript开发react时候引入 *.module.less有所不同*/
-const style = require('./index.module.css').default
+import { useState } from "react";
 
-export default function Index(){
-  return(
+/*使用typescript开发react时候引入 *.module.less有所不同*/
+
+const style = require("./index.module.css").default;
+
+export default function Index() {
+  let [state, setState] = useState(0);
+  return (
     <>
-      <div className={style.login}>Login <span className={style.name}>123</span></div>
+      <div className={style.login}>
+        Login <span className={style.name}>123</span>
+      </div>
       <div className={style.fa}>123</div>
+      <div onClick={() => setState(state + 1)}>{state}</div>
     </>
-  )
+  );
 }
